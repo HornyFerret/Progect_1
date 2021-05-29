de1ca9b33fa728c9c297303e1f45eca0d3103599
Kralizets <Kralizets@outlook.com>
2021-05-29 04:17:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Progect_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("are are ~~~");
+        }
+    }
+}
